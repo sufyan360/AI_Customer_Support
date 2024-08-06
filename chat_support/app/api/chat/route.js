a277841788f902d5cc62f0ba4f2febed6c2b6198
sufyan360 <117491069+sufyan360@users.noreply.github.com>
2024-08-06 00:21:48 -0500

    adding openrouter api to make it work

--- chat_support/app/api/chat/route.js
+++ chat_support/app/api/chat/route.js
@@ -1,6 +1,22 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server'
+import { OpenAI } from 'openai' 
 
-export default function POST(req) {
-    console.log('POST /api/chat');
-    return NextResponse.json({message: 'Hello from the server'});
-};
+// System prompt for the AI, providing guidelines on how to respond to users
+const systemPrompt = 'helpful ai assistant'
+
+// POST function to handle incoming requests
+export async function POST(req) {
+  const openai = new OpenAI({
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.OPENROUTER_API_KEY,
+  }) 
+  const data = await req.json() 
+
+  const completion = await openai.chat.completions.create({
+    messages: [{role: 'system', content: systemPrompt}, ...data],
+    model: 'openai/gpt-3.5-turbo',
+  })
+
+  console.log(completion.choices[0].message.content)
+  return NextResponse.json({message: 'Hello from server'})
+}
