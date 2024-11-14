ef34f30c4022ddcf735808b728fb118888412fde
sufyan360 <117491069+sufyan360@users.noreply.github.com>
2024-11-13 19:31:03 -0600

    change model

--- chat_support/app/api/chat/route.js
+++ chat_support/app/api/chat/route.js
@@ -19,7 +19,7 @@ export async function POST(req) {
   try {
     const completion = await openai.chat.completions.create({
       messages: [{ role: 'system', content: systemPrompt }, ...data],
-      model: 'gpt-3.5-turbo',
+      model: 'meta-llama/llama-3.1-70b-instruct:free',
       stream: true
     });
 
